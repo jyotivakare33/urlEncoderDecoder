e9f1abe5d56b79567d5a938d6bea26a83bf9fabc
Jyoti Vakare <jyotivakare33@gmail.com>
2021-01-21 08:21:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+function urlEncode(url) {
+  //some logic
+  return encodeURIComponent(url);
+}
+
+function urlDecode(url) {
+  //some logic
+  var uri_enc = encodeURIComponent(url);
+  var uri_dec = decodeURIComponent(uri_enc);
+  return uri_dec;
+}
+
+
+const readLineSync = require('readline-sync');
+
+const selectedUtilityOption = parseInt(readLineSync.question('Welcome to URL Encoder/Decoder. Which utility function would you like to use?\n1. urlEncode\n2. urlDecode\n'));
+
+console.log(`You selected ${selectedUtilityOption}`);
+
+switch(selectedUtilityOption) {
+  case 1:
+    const urlToEncode = readLineSync.question('Please enter the url to be encoded\n');
+    const encodedURL = urlEncode(urlToEncode);
+    console.log(encodedURL);
+    break;
+  case 2:
+    const urlToDecode = readLineSync.question('Please enter the url to be decoded\n');
+    const decodedURL = urlDecode(urlToDecode);
+    console.log(decodedURL);
+    break;
+  default:
+    console.log('Invalid URL\n');
+    break;
+}
